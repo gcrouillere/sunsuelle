9e8070040bfda68af3f6d5b2cae803309bab7fac
guillaume crouillere <gcrouillere@hotmail.fr>
2018-10-22 11:02:01 +0200

    position and visual updates

--- app/admin/ceramique.rb
+++ app/admin/ceramique.rb
@@ -79,36 +79,45 @@ show do |ceramique|
     end
  end
 
- controller do
+  controller do
 
-  def create
-    super do |format|
-      if resource.valid?
-        flash[:notice] = "Produit mis à jour"
-        redirect_to admin_produits_path and return
-      else
-        flash[:alert] = "Certains champs ont été oubliés ou ne sont pas correctement remplis. Voir ci-dessous."
+    def create
+      super do |format|
+        if resource.valid?
+          product_positions_management
+          flash[:notice] = "Produit mis à jour"
+          redirect_to admin_produits_path and return
+        else
+          flash[:alert] = "Certains champs ont été oubliés ou ne sont pas correctement remplis. Voir ci-dessous."
+        end
       end
     end
-  end
 
-  def destroy
-    flash[:notice] = "#{ENV['MODEL'][0...-1].capitalize} supprimé"
-    super do |format|
-      redirect_to admin_produits_path and return
-    end
-  end
-
-  def update
-    super do |format|
-      if resource.valid?
-        flash[:notice] = "Produit mis à jour"
+    def destroy
+      flash[:notice] = "#{ENV['MODEL'][0...-1].capitalize} supprimé"
+      super do |format|
         redirect_to admin_produits_path and return
-      else
-        flash[:alert] = "Certains champs ont été oubliés ou ne sont pas correctement remplis. Voir ci-dessous."
       end
     end
-  end
+
+    def update
+      super do |format|
+        if resource.valid?
+          product_positions_management
+          flash[:notice] = "Produit mis à jour"
+          redirect_to admin_produits_path and return
+        else
+          flash[:alert] = "Certains champs ont été oubliés ou ne sont pas correctement remplis. Voir ci-dessous."
+        end
+      end
+    end
+
+    def product_positions_management
+      if params[:ceramique][:position].present?
+        products_to_manage = Ceramique.where("position IS NOT NULL AND position >= ?", params[:ceramique][:position]).where.not(id: resource.id)
+        products_to_manage.each {|product| product.update(position: product.position + 1)}
+      end
+    end
 
   end
 
