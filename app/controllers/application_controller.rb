9064add58e1e0b2008c73de8548da212ea3c99b0
guillaume crouillere <gcrouillere@hotmail.fr>
2018-10-31 11:27:39 +0100

    locale according to tld

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,17 @@ class ApplicationController < ActionController::Base
   after_action :store_location
 
   def default_url_options
-  { host: ENV["HOST"] || "localhost:3000", locale: I18n.locale }
+  { host: ENV["HOST"] || "localhost:3000" }
   end
 
   def set_locale
-    if params[:change].present? || params[:locale].present?
-      I18n.locale = params[:locale]
-    else
-      locale_trial = extract_locale_from_accept_language_header
-      ["fr", "en"].include? locale_trial ? I18n.locale = locale_trial : I18n.locale = "en"
-    end
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+    # if params[:change].present? || params[:locale].present?
+    #   I18n.locale = params[:locale]
+    # else
+    #   locale_trial = extract_locale_from_accept_language_header
+    #   ["fr", "en"].include? locale_trial ? I18n.locale = locale_trial : I18n.locale = "en"
+    # end
   end
 
   def retrieve_admin
@@ -106,6 +107,11 @@ class ApplicationController < ActionController::Base
 
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first if request.env['HTTP_ACCEPT_LANGUAGE']
+  end
+
+  def extract_locale_from_tld
+    request.host.split('.').last == "com" ? parsed_locale = "en" : parsed_locale = "fr"
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 
   protected
