73fa0e33c8f967f5d53110c700ae506eed7f4308
guillaume crouillere <gcrouillere@gmail.com>
2019-08-21 14:32:06 +0200

    stripe payment process update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_205624) do
+ActiveRecord::Schema.define(version: 2019_08_21_121915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,6 +179,8 @@ ActiveRecord::Schema.define(version: 2018_11_30_205624) do
     t.integer "weight"
     t.bigint "promo_id"
     t.string "method"
+    t.string "stripe_session"
+    t.string "stripe_payment_intent"
     t.index ["lesson_id"], name: "index_orders_on_lesson_id"
     t.index ["promo_id"], name: "index_orders_on_promo_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
